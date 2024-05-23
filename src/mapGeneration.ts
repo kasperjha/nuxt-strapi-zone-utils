f1ff816564c3f2fdb17bbdb897bd346aa168925d
Kasper JH. Alfarnes <49387825+kakka0903@users.noreply.github.com>
2024-05-23 15:47:07 +0200

    fix: empty component map throws error

--- src/mapGeneration.ts
+++ src/mapGeneration.ts
@@ -11,12 +11,12 @@ export default function generateFileContents(relevantComponents: HookComponent[]
     .map(c => c.pascalName)
     .join(', ')
   const blockName = (c: HookComponent) => `${prefix.toLowerCase()}.${c.kebabName.slice(prefix.length + 1)}`
-  const buildMapRows = () => relevantComponents.map(c => `['${blockName(c)}', ${c.pascalName}]`).join(',\n  ')
+  const buildMapRows = () => relevantComponents.map(c => `['${blockName(c)}', ${c.pascalName}]`).join(',\n  ') + ',\n'
   const mapRows = (relevantComponents.length !== 0) ? buildMapRows() : ''
   return ''
     + 'import { type Component } from \'@nuxt/schema\'\n'
     + 'import { ' + importNames + ' } from \'#components\'\n\n'
     + 'export default new Map<string, Component>([\n  '
     + mapRows
-    + ',\n])\n'
+    + '])\n'
 }
